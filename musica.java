75f17d23db6211bcbf690e79ab15e343c5a2ad96
mari <117183854+cavmari@users.noreply.github.com>
2024-04-10 19:12:19 -0300

    musica.java

---
+++ musica.java
@@ -0,0 +1,23 @@
+public class musica {
+    private String titulo;
+    private String artista;
+
+    public Musica(String titulo, String artista) {
+        this.titulo = titulo;
+        this.artista = artista;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    @Override
+    public String toString() {
+        return "'" + titulo + "' por " + artista;
+    }
+}
+
